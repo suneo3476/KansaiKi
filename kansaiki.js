56ec9f6cb65aba617f341d70261345f8fc91e70e
Masato Fujimori <suneo3476@gmail.com>
2014-12-07 05:34:37 +0900

    配置される装備がおかしいけど原型

--- kansaiki.js
+++ kansaiki.js
@@ -1,18 +1,34 @@
+var util = require('util');
 /* 提督は出撃情報、艦娘、艦載機・水上機を教えて下さい*/
 
-/* 出撃するマップ */
+/* どこに出撃しますか？ */
 /* 例：2-2 */
 var attackMap = "5-2";
 
-/* 希望制空権 */
-/* 制空権確保:true,航空優勢:false */
-var isGetSky = true;
+/* 開幕航空戦での制空権はどうしますか？ */
+/* 制空権確保:0,航空優勢:1 */
+var getSkyLevel = 0;
+
+/* 被撃墜分を考慮してどのぐらい制空戦力を底上げしますか？ */
+/* デフォルトは10でいいと思いますが適宜調整してください */
+var biasForce = 10;
+
+/* 艦戦以外を積むスロットを1隻あたり最大でいくつ残しますか？ */
+/* 1~2が推奨です。0はボーキが死にます */
+var numOfExtraSlot = 1;
+
+/* 彩雲or二式艦上偵察機を載せますか？ */
+/* 両機載せる:2 *//* 片方だけ:1 *//* 載せない:0 */
+var numOfSpyPlanes = 1;
+
+/* 艦載機の数が多いスロットを1隻あたりいくつ節約しますか？ */
+/* する:数字　しない:0 */
+var numOfSavingSlot = 1;
 
 /* 艦種ごとの編成に入れる隻数 */
 /* 以下を満たして設定してください */
-
-var numOfSekikubo = 0;	/*正規空母*/ /*装甲空母は正規空母にカウントします*/
-var numOfKeikubo = 6;	/*軽空母*/
+var numOfSekikubo = 3;/*正規空母*/ /*装甲空母は正規空母にカウントします*/
+var numOfKeikubo = 0;	/*軽空母*/
 
 var numOfKokusen = 0;	/*航空戦艦*/
 var numOfKokujun = 0;	/*航空巡洋艦*/
@@ -54,10 +70,10 @@ var kokukanInput = {
 var kokukanMust = {
 	"赤城":0,	"赤城改":0,
 	"加賀":0,	"加賀改":0,
-	"蒼龍":0,	"蒼龍改":0,	 	"蒼龍改二": 		0,
-	"飛龍":0,	"飛龍改":0,	 	"飛龍改二": 		0,
+	"蒼龍":0,	"蒼龍改":0,	 	"蒼龍改二": 		1,
+	"飛龍":0,	"飛龍改":0,	 	"飛龍改二": 		1,
 	"翔鶴":0,	"翔鶴改":0,
-	"瑞鶴":0,	"瑞鶴改":0,
+	"瑞鶴":0,	"瑞鶴改":1,
 	"雲龍":0,	"雲龍改":0,
 	"大鳳":0,	"大鳳改":0,
 	"鳳翔":0,	"鳳翔改":0,
@@ -234,6 +250,30 @@ var kansaikiAA = {
 	"瑞雲12型":3,
 	"瑞雲12型(六三四空)":3
 }
+
+/* 艦載機・水上機のリスト */
+var allKansen = [
+	"九六式艦戦",
+	"零式艦戦21型",	"零式艦戦21型(熟練)",
+	"零式艦戦52型",	"零式艦戦52型丙(六〇一空)",
+	"紫電改二",
+	"烈風",	"烈風(六〇一空)",	"烈風改",
+	"震電改",
+];
+var allKankou = [
+	"九七式艦攻(友永隊)",
+	"天山一二型(友永隊)",
+];
+var allKanbaku = [
+	"零式艦戦62型(爆戦)",
+	"九九式艦爆(熟練)",
+	"彗星(江草隊)",
+];
+var allSuibaku = [
+	"瑞雲",		"瑞雲(六三四空)",
+	"瑞雲12型",	"瑞雲12型(六三四空)"
+];
+
 /* マップの航空優勢・制空権確保にあたる制空値 */
 /* "マップ":[航空優勢,制空権確保] */
 /* AA = Anti-Aircraft */
@@ -309,6 +349,19 @@ function isKokujun(str){
 	return allKokujun.indexOf(str) != -1;
 }
 
+/* ある装備がその種類に該当するかどうか */
+function isKansen(str){
+	return allKansen.indexOf(str) != -1;
+}
+function isKankou(str){
+	return allKankou.indexOf(str) != -1;
+}
+function isKanbaku(str){
+	return allKanbaku.indexOf(str) != -1;
+}
+function isSuibaku(str){
+	return allSuibaku.indexOf(str) != -1;
+}
 /* n個の要素を持つ配列からr個の要素の組合せの配列を返す */
 /*　folk from http://taichino.com/programming/3355 */
 Array.prototype.combinations = function(r) {
@@ -378,7 +431,8 @@ for(var key in kokukanMust){
 	if(kokukanMust[key] == 1){
 		var obj = {};
 		obj['name'] = key;
-		obj['slot'] = kokukanSlot[key];
+		obj['slot'] = kokukanSlot[key].concat();
+		obj['exSlot'] = [];
 		must.push(obj);
 		for(var key2 in kokukanInput){
 			if(checkIdentify(key,key2)){
@@ -393,7 +447,29 @@ for(var key in kokukanMust){
 	}
 }
 
-//console.log(kokukanInput);
+/* 必須艦に同一艦種がないか調べる */
+for(var i=0; i<must.length-1; i++){
+	for(var j=i+1; j<must.length; j++){
+		if(checkIdentify(must[i]['name'],must[j]['name'])){
+			console.log('必須感に指定された'+must[i]['name']+'と'+must[j]['name']+'は同一の艦娘です\n必須感に同一艦を指定しないでください');
+		}
+	}
+}
+
+/* 必須艦娘と指定した艦数が矛盾した場合終了 */
+if(numOfSekikubo<0){
+	console.log("必須艦数の数が正規空母の数より大きいです\nnumOfSekikuboを修正してください");
+	process.exit();
+}else if(numOfKeikubo<0){
+	console.log("必須艦数の数が軽空母の数より大きいです\nnumOfKeikuboを修正してください");
+	process.exit();
+}else if(numOfKokusen<0){
+	console.log("必須艦数の数が航空戦艦の数より大きいです\nnumOfKokusenを修正してください");
+	process.exit();
+}else if(numOfKokujun<0){
+	console.log("必須艦数の数が航空巡洋艦の数より大きいです\nnumOfKokujunを修正してください");
+	process.exit();
+}
 
 /* 入力より出撃可能な艦娘の一覧を作る */
 var kokukan = [];
@@ -401,7 +477,8 @@ for(var key in kokukanInput){
 	if(kokukanInput[key] == 1){
 		var obj = {};
 		obj['name'] = key;
-		obj['slot'] = kokukanSlot[key];
+		obj['slot'] = kokukanSlot[key].concat();
+		obj['exSlot'] = [];
 		kokukan.push(obj);
 	}
 }
@@ -418,7 +495,6 @@ for(var key in sekikuboCombi){
 		}
 	}
 }
-// console.log(sekikuboCombi);
 
 var keikubo = kokukan.filter(function(value, index){
 	return isKeikubo(value['name']);
@@ -432,7 +508,6 @@ for(var key in keikuboCombi){
 		}
 	}
 }
-// console.log(keikuboCombi);
 
 var kokusen = kokukan.filter(function(value, index){
 	return isKokusen(value['name']);
@@ -446,7 +521,6 @@ for(var key in kokusenCombi){
 	}
 }
 var kokusenCombi = kokusen.combinations(numOfKokusen);
-// console.log(kokusenCombi);
 
 var kokujun = kokukan.filter(function(value, index){
 	return isKokujun(value['name']);
@@ -460,47 +534,170 @@ for(var key in kokujunCombi){
 	}
 }
 var kokujunCombi = kokujun.combinations(numOfKokujun);
-// console.log(kokujunCombi);
 
 /* 艦種ごとの組合せを統合した組合せ */
 var kokukanCombi = [];
-var sekiCL = sekikuboCombi.length;
+var sekiCL = sekikuboCombi.length
 var keiCL = keikuboCombi.length;
 var senCL = kokusenCombi.length;
 var junCL = kokujunCombi.length;
-if(sekiCL == 0)	sekiCL = 1;	if(keiCL == 0)	keiCL = 1;
-if(senCL == 0)	senCL = 1;	if(junCL == 0)	junCL = 1;
+if(sekiCL == 0)	sekiCL = 1;
+if(keiCL == 0)	keiCL = 1;
+if(senCL == 0)	senCL = 1;
+if(junCL == 0)	junCL = 1;
 for(var seki=0; seki<=sekiCL; seki++){
-for(var kei=0; kei<=keiCL; kei++){
-for(var sen=0; sen<=senCL; sen++){
-for(var jun=0; jun<=junCL; jun++){
-	if(sekikuboCombi[seki]==undefined||keikuboCombi[kei]==undefined||kokusenCombi[sen]==undefined||kokujunCombi[jun]==undefined)	continue;
-	if(seki==sekiCL||kei==keiCL||sen==senCL||jun==junCL)	continue;
-	kokukanCombi.push([].concat(must).concat(sekikuboCombi[seki]).concat(keikuboCombi[kei]).concat(kokusenCombi[sen]).concat(kokujunCombi[jun]));
-}}}}
-
+	for(var kei=0; kei<=keiCL; kei++){
+		for(var sen=0; sen<=senCL; sen++){
+			for(var jun=0; jun<=junCL; jun++){
+				if(sekikuboCombi[seki]==undefined||keikuboCombi[kei]==undefined||kokusenCombi[sen]==undefined||kokujunCombi[jun]==undefined)	continue;
+				if(seki==sekiCL||kei==keiCL||sen==senCL||jun==junCL)	continue;
+				var result = [].concat(must).concat(sekikuboCombi[seki]).concat(keikuboCombi[kei]).concat(kokusenCombi[sen]).concat(kokujunCombi[jun]);
+				kokukanCombi.push(result);
+			}
+		}
+	}
+}
 /* 入力より装備可能な装備の一覧を作る */
-var kansaiki = [];
+var allKansaiki = [];
 for(var key in kansaikiInput){
 	if(kansaikiInput[key] > 0){
 		for(var i=0; i<kansaikiInput[key]; i++){
 			var obj = {};
 			obj['name'] = key;
 			obj['AA'] = kansaikiAA[key];
-			kansaiki.push(obj);
+			allKansaiki.push(obj);
 		}
 	}
 }
-kansaiki.sort(function (a,b){
+allKansaiki.sort(function (a,b){
 	var x = a['AA'];
 	var y = b['AA'];
 	return y-x;
 })
+var kansen = allKansaiki.filter(function(elem){
+	return isKansen(elem['name']);
+});
+var kankou = allKansaiki.filter(function(elem){
+	return isKankou(elem['name']);
+});
+var kanbaku = allKansaiki.filter(function(elem){
+	return isKanbaku(elem['name']);
+});
+var suibaku = allKansaiki.filter(function(elem){
+	return isSuibaku(elem['name']);
+});
+
+/* 目標とする制空戦力 */
+var requireForce = mapAA[attackMap][getSkyLevel];
+
+/* 組合せごとに最適な装備の組合せを探していく　*/
+for(var i=0; i<kokukanCombi.length; i++){
+	var combi = [];
+	combi = combi.concat(kokukanCombi[i]);
+	var kansaiki = [];
+	kansaiki = kansaiki.concat(allKansaiki);
+	// console.log(combi);
+	// console.log(kansaiki);
+	/* 艦偵の数に応じてスロットを削減する */
+	if(numOfSpyPlanes>0){
+		var minSlot = [100,100,100,100,100,100];
+		for(var j=0; j<combi.length; j++){
+			for(var s=0; s<4; s++){
+				var numOfPlanes = combi[j]['slot'][s];
+				if(numOfPlanes < minSlot[j] && numOfPlanes > 0)
+					minSlot[j] = numOfPlanes;
+			}
+		}
+		switch(numOfSpyPlanes){
+			case 2:
+			for(var j=0; j<combi.length; j++){
+				for(var s=0; s<4; s++){
+					var numOfPlanes = combi[j]['slot'][s];
+					if(numOfPlanes == minSlot)
+						combi[j]['slot'][s] = -2;
+				}
+			}
+			break;
+			case 1:
+			var deleteSlot = Math.min.apply(null,minSlot);
+			findMinSlot: for(var j=0; j<combi.length; j++){
+				for(var s=0; s<4; s++){
+					var numOfPlanes = combi[j]['slot'][s];
+					if(numOfPlanes == deleteSlot && numOfPlanes > 0){
+						combi[j]['slot'][s] = -2;
+						break findMinSlot;
+					}
+				}
+			}
+			break;
+			default :
+			break;
+		}
+	}
+	/* 節約するスロットを削減する */
+	if(numOfSavingSlot > 0){
+		var maxSlot = [];
+		for(var j=0; j<combi.length; j++)
+			maxSlot[j] = combi[j]['slot'].sort(function(a,b){return a<b;});
+		while(numOfSavingSlot-- > 0){
+			findSavingSlot:
+			for(var j=0; j<combi.length; j++){
+				for(var s=0; s<4; s++){
+					var numOfPlanes = combi[j]['slot'][s];
+					if(numOfPlanes == maxSlot[j][s] && numOfPlanes > 0){
+						combi[j]['slot'][s] = -1;
+						break findSavingSlot;
+					}
+				}
+			}
+		}
+	}
+
+	/* 艦載機の数が多いスロットから装備を埋めていく */
+	var currentForce = 0;
+	var result = [];
+	result = result.concat(kokukanCombi[i]);
+	while(currentForce < requireForce && kansaiki.length > 0){
+		//艦娘ごとに一番艦載機の数が多いスロットを1つ選ぶ
+		var maxSlot = [0,0,0,0,0,0];
+		for(var j=0; j<combi.length; j++){
+			for(var s=0; s<4; s++){
+				var numOfPlanes = combi[j]['slot'][s];
+				if(numOfPlanes > maxSlot[j] && numOfPlanes > 0)
+					maxSlot[j] = numOfPlanes;
+			}
+		}
+		var numOfTargetPlanes = Math.max.apply(null,maxSlot);
+		//艦娘のなかで一番艦載機の数が多いスロットを除外する
+		findTargetSlot:
+		for(var j=0; j<combi.length; j++){
+			for(var s=0; s<4; s++){
+				var numOfPlanes = combi[j]['slot'][s];
+				if(numOfPlanes == numOfTargetPlanes){
+					combi[j]['slot'][s] = 0;
+					result[j]['name'] = combi[j]['name'];
+					result[j]['slot'] = ['','','',''];
+					result[j]['slot'][s] = kansaiki.slice(0,1)[0]['name'];
+					break findTargetSlot;
+				}
+			}
+		}
+		currentForce += Math.sqrt(numOfTargetPlanes)*(kansaiki.shift())['AA'];
+	}
+	console.log('編成');
+	for(var j=0; j<combi.length; j++){
+		console.log((j+1)+" "+combi[j]['name']);
+		for(var s=0; s<4; s++){
+			console.log(" Slot"+(s+1)+" "+combi[j]['slot'][s]);
+		}
+	}
+	console.log("制空戦力\t"+currentForce+"\n");
+}
 
 //組み合わせ一覧
-console.log(kokukanCombi);
+// console.log(kokukanCombi);
 //残り装備
-console.log(kansaiki);
+ console.log(kansaiki);
 
 
 
